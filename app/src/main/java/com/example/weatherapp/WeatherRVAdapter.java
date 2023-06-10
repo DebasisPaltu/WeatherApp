c496612e6be4132bd2d358410971228749b7680a
Beher <behera.debasis@outlook.com>
2023-06-10 15:56:47 +0530

    error fix

--- app/src/main/java/com/example/weatherapp/WeatherRVAdapter.java
+++ app/src/main/java/com/example/weatherapp/WeatherRVAdapter.java
@@ -30,25 +30,25 @@ public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.View
     @NonNull
     @Override
     public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-       View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item,parent,false);
-       return new ViewHolder(view);
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
 
         WeatherRVModal modal = weatherRVModalArrayList.get(position);
-        holder.tempretureTV.setText(modal.getTempreature()+"°c");
+        holder.tempretureTV.setText(modal.getTempreature() + "°c");
         Picasso.get().load("http:".concat(modal.getIcon())).into(holder.conditionTV);
         holder.windTV.setText(modal.getWindSpeed());
-        holder.timeTV.setText(modal.getTime()+"Km/h");
+        holder.timeTV.setText(modal.getTime() + "Km/h");
 
         SimpleDateFormat input = new SimpleDateFormat("YYYY-MM-dd  hh:mm");
         SimpleDateFormat output = new SimpleDateFormat("hh:mm  aa");
-        try{
+        try {
             Date t = input.parse(modal.getTime());
             holder.timeTV.setText(output.format(t));
-        }catch (ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
@@ -60,14 +60,17 @@ public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.View
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView windTV,tempretureTV,timeTV;
+        TextView windTV;
+        TextView tempretureTV;
+        TextView timeTV;
         private ImageView conditionTV;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            windTV=itemView.findViewById(R.id.idWindSpeed);
-            tempretureTV=itemView.findViewById(R.id.idTVTempreture);
-            timeTV=itemView.findViewById(R.id.idTVTime);
-            conditionTV=itemView.findViewById(R.id.idTVCondition);
+            windTV = itemView.findViewById(R.id.idWindSpeed);
+            tempretureTV = itemView.findViewById(R.id.idTVTemprature);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            conditionTV = itemView.findViewById(R.id.idIVCondition);
 
         }
     }
